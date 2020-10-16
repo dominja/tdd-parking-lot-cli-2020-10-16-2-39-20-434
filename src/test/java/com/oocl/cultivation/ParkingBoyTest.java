fa809c209d5e805c33e489bd5dfc4bdec239dcc5
Janelle <dominja@oocl.com>
2020-10-16 14:01:47 +0800

    [ParkingBoy][Janelle][Test]: added test case for returning the ticket when given car

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,17 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
-
+    void should_return_parking_ticket_when_park_given_car_to_boy() {
+        //given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        // when
+        ParkingTicket ticket = parkingBoy.park(car);
+        //then
+        assertNotNull(ticket);
     }
 }
