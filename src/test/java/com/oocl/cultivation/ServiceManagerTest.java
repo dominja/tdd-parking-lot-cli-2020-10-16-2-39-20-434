d7b612541a696a29d8c367fed4477bf0de475a43
Janelle <dominja@oocl.com>
2020-10-17 19:53:25 +0800

    [ParkingBoy][Janelle][Test]: added test for sevice manager when adding parking boys to the list

---
+++ src/test/java/com/oocl/cultivation/ServiceManagerTest.java
@@ -0,0 +1,23 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ServiceManagerTest {
+    @Test
+    void should_return_list_of_parking_boys_to_management_list_manage_parking_boys() {
+        //Given
+        List<ParkingBoy> parkingBoys;
+        ServiceManager serviceManager = new ServiceManager();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot());
+        List<ParkingBoy> listParkingBoy = Arrays.asList(parkingBoy, smartParkingBoy);
+        //When
+        parkingBoys=serviceManager.addParkingBoy(listParkingBoy);
+        // Then
+        Assertions.assertNotNull(parkingBoys);
+    }
+}
