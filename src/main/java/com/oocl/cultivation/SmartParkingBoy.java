eaff185d6c44336927a82dbce18e55cb283011b2
Janelle <dominja@oocl.com>
2020-10-17 17:57:35 +0800

    [ParkingBoy][Janelle][Feature]: added pickParkingLot method for SmartParkingBoy

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,17 +1,25 @@
 package com.oocl.cultivation;
 
+import java.util.Comparator;
 import java.util.List;
 
-public class SmartParkingBoy {
+public class SmartParkingBoy extends ParkingBoy {
+    private List<ParkingLot> listParkingLots;
+
     public SmartParkingBoy(ParkingLot parkingLot) {
-
+        super(parkingLot);
     }
 
+    @Override
+    public ParkingLot pickParkingLot() {
+        return listParkingLots.stream()
+                .max(Comparator.comparing(ParkingLot::getAvailableSlot))
+                .orElseThrow(() -> new NoAvailableSpacesException("Not Enough Position"));
+    }
+
+    @Override
     public void setListParkingLots(List<ParkingLot> parkingLots) {
-
+        this.listParkingLots = parkingLots;
     }
 
-    public void park(Car carsNew) {
-
-    }
 }
