c680498ec8e62d872acb61cdf9ba29047416155d
Janelle <dominja@oocl.com>
2020-10-17 18:11:24 +0800

    [ParkingBoy][Janelle][Feature]: added class for SuperSmartParkingBoy

---
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -0,0 +1,16 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SuperSmartParkingBoy {
+    public SuperSmartParkingBoy(ParkingLot parkingLot) {
+
+    }
+
+    public void setListParkingLots(List<ParkingLot> parkingLots) {
+    }
+
+    public void park(Car carsNew) {
+
+    }
+}
