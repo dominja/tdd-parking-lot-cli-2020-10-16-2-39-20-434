63b7926d273436efcef2e9270f25aaa5ede1e55c
Janelle <dominja@oocl.com>
2020-10-17 20:27:32 +0800

    [ParkingBoy][Janelle][Test]: added test case for ordering parkboy on list to park a car to his managed parking lot

--- src/test/java/com/oocl/cultivation/ServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ServiceManagerTest.java
@@ -16,8 +16,30 @@ class ServiceManagerTest {
         SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot());
         List<ParkingBoy> listParkingBoy = Arrays.asList(parkingBoy, smartParkingBoy);
         //When
-        parkingBoys=serviceManager.getParkingBoyList(listParkingBoy);
+        parkingBoys = serviceManager.getParkingBoyList(listParkingBoy);
         // Then
         Assertions.assertNotNull(parkingBoys);
+    }
+
+    @Test
+    void should_return_parking_ticket_when_park_given_car_to_boy_which_is_on_list_and_parking_lot_is_managed() {
+        //Given
+        Car car = new Car();
+        ParkingTicket ticket;
+        ServiceManager serviceManager = new ServiceManager();
+        ParkingBoy normalParkingBoy = new ParkingBoy(new ParkingLot());
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot());
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(3);
+
+        List<ParkingLot> parkingLots = Arrays.asList(parkingLot1, parkingLot2);
+        normalParkingBoy.setListParkingLots(parkingLots);
+
+        List<ParkingBoy> listParkingBoy = Arrays.asList(normalParkingBoy, smartParkingBoy);
+        serviceManager.getParkingBoyList(listParkingBoy);
+        //When
+        ticket = serviceManager.orderParkingBoyToPark(car, normalParkingBoy);
+        // Then
+        Assertions.assertNotNull(ticket);
     }
 }
