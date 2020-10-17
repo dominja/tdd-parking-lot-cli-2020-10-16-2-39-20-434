01bfb7a0151518171987a7bb19a9aac3e44c8955
Janelle <dominja@oocl.com>
2020-10-17 19:01:48 +0800

    [ParkingBoy][Janelle][Feature]: added logic for SuperSmartParkingBoy

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -2,15 +2,23 @@ package com.oocl.cultivation;
 
 import java.util.List;
 
-public class SuperSmartParkingBoy {
+public class SuperSmartParkingBoy extends ParkingBoy {
+    private List<ParkingLot> listParkingLots;
+
     public SuperSmartParkingBoy(ParkingLot parkingLot) {
-
+        super(parkingLot);
     }
 
+    @Override
+    public ParkingLot pickParkingLot() {
+        return listParkingLots.stream().reduce((parkingLot, parkingLot2) -> parkingLot.getAverageAvailableSlot() >
+                parkingLot2.getAverageAvailableSlot() ? parkingLot : parkingLot2)
+                .orElseThrow(() -> new NoAvailableSpacesException("Not Enough Position"));
+    }
+
+    @Override
     public void setListParkingLots(List<ParkingLot> parkingLots) {
+        this.listParkingLots = parkingLots;
     }
 
-    public void park(Car carsNew) {
-
-    }
 }
