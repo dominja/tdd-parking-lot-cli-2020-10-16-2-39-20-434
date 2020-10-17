5deaeb5476545d492187a19c56e218f715813678
Janelle <dominja@oocl.com>
2020-10-17 19:55:41 +0800

    [ParkingBoy][Janelle][Feature]: added class for ServiceManager

---
+++ src/main/java/com/oocl/cultivation/ServiceManager.java
@@ -0,0 +1,10 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class ServiceManager {
+
+    public List<ParkingBoy> addParkingBoy(List<ParkingBoy> listParkingBoy) {
+        return null;
+    }
+}
