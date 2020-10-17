9cab9f6438d9d648884cda3abe1943f41b12686e
Janelle <dominja@oocl.com>
2020-10-17 18:59:34 +0800

    [ParkingBoy][Janelle][Re-factor]: enhanced test case for super smart boy

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SuperSmartParkingBoyTest {
+class SuperSmartParkingBoyTest {
     @Test
     void should_return_number_of_parked_cars_when_parked_give_two_parking_lot_to_super_smart_boy() {
         //given
@@ -26,7 +26,7 @@ public class SuperSmartParkingBoyTest {
         int actual1 = parkingLot1.getParkedCars().size();
         int actual2 = parkingLot2.getParkedCars().size();
         //then
-        assertEquals(2, actual1);
-        assertEquals(5, actual2);
+        assertEquals(1, actual1);
+        assertEquals(6, actual2);
     }
 }
