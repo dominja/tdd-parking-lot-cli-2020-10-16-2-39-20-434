445cfeb0cfe217ed40b08905ff57232e582a9a6f
Janelle <dominja@oocl.com>
2020-10-17 20:30:48 +0800

    [ParkingBoy][Janelle][Feature]: added orderParkingBoyToPark method

--- src/main/java/com/oocl/cultivation/ServiceManager.java
+++ src/main/java/com/oocl/cultivation/ServiceManager.java
@@ -3,11 +3,12 @@ package com.oocl.cultivation;
 import java.util.List;
 
 public class ServiceManager {
-    
+
     public List<ParkingBoy> getParkingBoyList(List<ParkingBoy> parkingBoyList) {
         return parkingBoyList;
     }
 
-
-
+    public ParkingTicket orderParkingBoyToPark(Car car, ParkingBoy parkingBoy) {
+        return null;
+    }
 }
