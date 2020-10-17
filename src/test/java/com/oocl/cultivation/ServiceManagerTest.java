02782f07fb4600e532d51b414069d38e210a34a5
Janelle <dominja@oocl.com>
2020-10-17 21:17:48 +0800

    [ParkingBoy][Janelle][Test]: added test case when asked boy to park only on his own parking lot

--- src/test/java/com/oocl/cultivation/ServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ServiceManagerTest.java
@@ -1,6 +1,5 @@
 package com.oocl.cultivation;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -44,6 +43,7 @@ class ServiceManagerTest {
         // Then
         assertNotNull(ticket);
     }
+
     @Test
     void should_return_null_when_park_given_car_to_boy_which_is_not_on_list_and_parking_lot_is_managed() {
         //Given
@@ -65,5 +65,38 @@ class ServiceManagerTest {
         ticket = serviceManager.orderParkingBoyToPark(car, smartParkingBoy);
         // Then
         assertNull(ticket);
+    }
+    @Test
+    void should_return_right_count_of_parking_lot_slot_when_park_given_to_boy_and_can_only_access_his_own_parkinglot() {
+        //Given
+        Car car = new Car();
+        ServiceManager serviceManager = new ServiceManager(new ParkingLot());
+
+        ParkingBoy normalParkingBoy1 = new ParkingBoy(new ParkingLot());
+        ParkingBoy normalParkingBoy2 = new ParkingBoy(new ParkingLot());
+
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(3);
+
+        ParkingLot parkingLot3 = new ParkingLot(3);
+        ParkingLot parkingLot4 = new ParkingLot(4);
+
+        List<ParkingLot> parkingLotsForNormalBoy1 = Arrays.asList(parkingLot1, parkingLot2);
+        List<ParkingLot> parkingLotsForNormalBoy2 = Arrays.asList(parkingLot3,parkingLot4);
+
+        normalParkingBoy1.setListParkingLots(parkingLotsForNormalBoy1);
+        normalParkingBoy2.setListParkingLots(parkingLotsForNormalBoy2);
+
+        List<ParkingBoy> listParkingBoy = Arrays.asList(normalParkingBoy1, normalParkingBoy2);
+        serviceManager.getParkingBoyList(listParkingBoy);
+
+
+        //When
+        serviceManager.orderParkingBoyToPark(car, normalParkingBoy1);
+        int countParkedCarsNormalBoy1=parkingLot1.getParkedCars().size();
+        int countParkedCarsNormalBoy2=parkingLot2.getParkedCars().size();
+        // Then
+        assertEquals(1,countParkedCarsNormalBoy1);
+        assertEquals(0,countParkedCarsNormalBoy2);
     }
 }
