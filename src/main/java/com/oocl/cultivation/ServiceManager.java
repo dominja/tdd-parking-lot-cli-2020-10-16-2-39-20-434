af52485b106c918f5418ef6a4f567a6500a805fe
Janelle <dominja@oocl.com>
2020-10-17 20:47:16 +0800

    [ParkingBoy][Janelle][Feature]: added logic for parking car ordered by service manager

--- src/main/java/com/oocl/cultivation/ServiceManager.java
+++ src/main/java/com/oocl/cultivation/ServiceManager.java
@@ -2,13 +2,22 @@ package com.oocl.cultivation;
 
 import java.util.List;
 
-public class ServiceManager {
+public class ServiceManager extends ParkingBoy {
+
+    private List<ParkingBoy> parkingBoyList;
+    public ServiceManager(ParkingLot parkingLot) {
+        super(parkingLot);
+    }
 
     public List<ParkingBoy> getParkingBoyList(List<ParkingBoy> parkingBoyList) {
-        return parkingBoyList;
+         this.parkingBoyList=parkingBoyList;
+         return this.parkingBoyList;
     }
 
     public ParkingTicket orderParkingBoyToPark(Car car, ParkingBoy parkingBoy) {
+         if(parkingBoyList.contains(parkingBoy)) {
+            return parkingBoy.park(car);
+         }
         return null;
     }
 }
